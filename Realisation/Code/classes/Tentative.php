ff4e60ad19d31ea7d96646a11a3e625133ed41cd
Alexis-Nicolas <80096662+Alexis-Nicolas@users.noreply.github.com>
2021-11-11 08:42:12 +0100

    Ajout de la classe Tentative

---
+++ Realisation/Code/classes/Tentative.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Tentative 
+ */
+class Tentative
+{  
+
+    /**
+     * Code
+     * 
+     * @var string
+     */
+    private $Code;
+
+    /**
+     * joueur 
+     * 
+     * @var Joueur
+     */
+    private Joueur $joueur;
+
+    public function __construct(string $Code){
+        $this->Code = $Code;
+    }
+
+    public function Tenter($Code) : Reponse {
+        return $Code;
+    }
+
+    
+}
+?>
