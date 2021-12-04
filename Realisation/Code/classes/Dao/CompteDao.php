19a7976c9006e39dc760e056ce5a223b5b5eaea7
rubenclerc <rubenclercp@gmail.com>
2021-12-04 15:02:09 +0100

    commit de transition

--- Realisation/Code/classes/Dao/CompteDao.php
+++ Realisation/Code/classes/Dao/CompteDao.php
@@ -15,15 +15,18 @@ class CompteDao{
         $username=$c->getUsername();
         $pass=$c->getPasswordHash();
         $admin=$c->getEstAdmin();
+
         $req = $this->db->prepare('INSERT INTO compte (username, passw, estAdmin) values (:username, :pass, :estAdmin)');
         $req->bindParam(':username',$username);
         $req->bindParam(':pass', $pass);	
         $req->bindParam(':estAdmin', $admin);
         $req->execute();
-        
     }
 
     public function Read(string $login, string $passHash): Compte{
+        $compte = null;
+        $row =null;
+
         if(isset($login)&&isset($passHash)){
             try{
                 $req=$this->db->prepare('SELECT * FROM COMPTE WHERE username=:username');
@@ -48,11 +51,14 @@ class CompteDao{
         $username=$c->getUsername();
         $pass=$c->getPasswordHash();
         $admin=$c->getEstAdmin();
-        $req=$this->db->prepare('SELECT * FROM COMPTE WHERE username=:username');
+
+        $req=$this->db->prepare('SELECT idCompte FROM COMPTE WHERE username=:username');
         $req->bindParam(':username',$username);
         $req->execute();
+
         $row=$req->fetch(PDO::FETCH_ASSOC);
         $id=$row['idCompte'];
+
         $req = $this->db->prepare('update compte set username = :username, passw = :pass, estAdmin = :estAdmin where idCompte = '. $id);
         $req->bindParam(':username',$username);
         $req->bindParam(':pass', $pass);	
