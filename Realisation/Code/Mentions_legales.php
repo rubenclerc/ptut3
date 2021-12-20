8e27be15f2b6c584edd6ea05c58de4a6fbbc5151
Alexis-Nicolas <alexis.nicolas@iut-dijon.u-bourgogne.fr>
2021-12-20 09:24:46 +0100

    CGU et mentions légales

---
+++ Realisation/Code/Mentions_legales.php
@@ -0,0 +1,23 @@
+<?PHP 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mentions légales</title>
+</head>
+<body>
+        <h2>Mentions légales</h2>
+            <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet <a href="https://www.MindMaster.com">https://www.MindMaster.com</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
+            </p><p><strong>Propriétaire</strong> :  SARL Koala Prod Capital social de 0€  – 7 Bd Dr Petitjean 21078 Dijon<br>                       
+            <strong>Responsable publication</strong> : Koala Prod – 06 00 00 00 00<br>
+            Le responsable publication est une personne physique ou une personne morale.<br>
+            <strong>Webmaster</strong> : Alexis NICOLAS – 0600000000<br>
+            <strong>Hébergeur</strong> : Gemalin – 12-14 Rond Point des Champs Elysées 75008 Paris 0 811 88 77 66<br>
+            <strong>Délégué à la protection des données</strong> : Koala Prod – Koala_Prod@Gemalin.com<br>
+            </p>
+    </body>
+</html>
