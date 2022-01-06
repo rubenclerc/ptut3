9b22a63888088734c7c9fb4129703d154bcd7ac4
rubenclerc <rubenclercp@gmail.com>
2022-01-06 16:15:00 +0100

    Compte

--- Realisation/Code/classes/Dao/CompteDao.php
+++ Realisation/Code/classes/Dao/CompteDao.php
@@ -17,7 +17,7 @@ class CompteDao{
         $pass=$c->getPasswordHash();
         $admin=$c->getEstAdmin();
 
-        $req = $this->db->prepare('INSERT INTO Compte (username, passw, estAdmin, nbPoints) VALUES (:username, :pass, :estAdmin, 0)');
+        $req = $this->db->prepare('INSERT INTO Compte(username, passw, estAdmin, nbPoints) VALUES(:username, :pass, :estAdmin, 0)');
         $req->bindParam(':username',$username);
         $req->bindParam(':pass', $pass);	
         $req->bindParam(':estAdmin', $admin);
