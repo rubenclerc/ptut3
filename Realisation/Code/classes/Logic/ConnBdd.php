2c1c50111d110a9bea8255222351e8cbe1f20ce3
rubenclerc <rubenclercp@gmail.com>
2022-01-06 17:35:19 +0100

    bdd

--- Realisation/Code/classes/Logic/ConnBdd.php
+++ Realisation/Code/classes/Logic/ConnBdd.php
@@ -2,7 +2,7 @@
 
 function Connexion():PDO{
     try{
-        $bdd=new PDO('mysql:host=localhost;dbname=ptut3', 'root', '');
+        $bdd=new PDO('mysql:host=localhost;dbname=grp-299_s3_prjtut', 'grp-299', 'YPZHFt7q');
     }
     catch(Exception $e){
         die("Erreur : ".$e->getMessage() );
